7d3f0d7b1d04a3c51e73fb510d6bce7dd25fb893
cfenogli <44930510+cfenogli@users.noreply.github.com>
2020-07-06 13:17:00 -0500

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -37,7 +37,7 @@ public class Menu
         Block_Chain genBlock = new Block_Chain();
 
         MongoClientURI uri = new MongoClientURI(
-        				"mongodb://DAVE:DAVE@cluster0-shard-00-00-0gdb9.mongodb.net:27017,cluster0-shard-00-01-0gdb9.mongodb.net:27017,cluster0-shard-00-02-0gdb9.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority");
+        				"mongodb://<UserName>:<Password>@cluster0-shard-00-00-0gdb9.mongodb.net:27017,cluster0-shard-00-01-0gdb9.mongodb.net:27017,cluster0-shard-00-02-0gdb9.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority");
         MongoClient mongoClient = new MongoClient(uri);
         MongoDatabase database = mongoClient.getDatabase("blockchain");
 
